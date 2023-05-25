640bd0b979a0dab280c9c09bff257867202995f2
gaby5u <gabrielaoloeri@gmail.com>
2023-05-25 10:05:58 +0300

    Add new sections

---
+++ landing-page/script.js
@@ -0,0 +1,8 @@
+let country, continent, population;
+country="Germany";
+continent="Europe";
+population=83;
+console.log (country+" "+continent+" "+population);
+let isIsland=false;
+let description=`${country} is in ${continent}, and it has ${population} million people`;
+console.log(description);
