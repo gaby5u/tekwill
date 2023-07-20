2bee5a448ad89260f063df66416c544ea892131a
gaby5u <gabrielaoloeri@gmail.com>
2023-07-20 20:17:28 +0300

    Add modal, search input

--- landing-page/realtor/script.js
+++ landing-page/realtor/script.js
@@ -1,38 +1,51 @@
 import { API_KEY } from "../env";
 
+const url = 'https://realtor.p.rapidapi.com/properties/v3/list';
+const options = {
+  method: 'POST',
+  headers: {
+    'content-type': 'application/json',
+    'X-RapidAPI-Key': API_KEY,
+    'X-RapidAPI-Host': 'realtor.p.rapidapi.com'
+  },
+  body:  JSON.stringify({
+    limit: 200,
+    offset: 0,
+    postal_code: '90004',
+    status: [
+      'for_sale',
+      'ready_to_build'
+    ],
+    sort: {
+      direction: 'desc',
+      field: 'list_date'
+    }
+  })
+};
 const fetchData = () => {
-  const url = 'https://realtor.p.rapidapi.com/properties/v3/list';
-  const options = {
-    method: 'POST',
-    headers: {
-      'content-type': 'application/json',
-      'X-RapidAPI-Key': API_KEY,
-      'X-RapidAPI-Host': 'realtor.p.rapidapi.com'
-    },
-    body:  JSON.stringify({
-      limit: 200,
-      offset: 0,
-      postal_code: '90004',
-      status: [
-        'for_sale',
-        'ready_to_build'
-      ],
-      sort: {
-        direction: 'desc',
-        field: 'list_date'
-      }
-    })
-  };
 
   const loadingSpinner = document.getElementById ('loading-spinner');
   loadingSpinner.style.display = 'block';
   
+  const searchContainer = document.getElementById ('search-container');
+  if (loadingSpinner.style.display === 'block') {
+    searchContainer.style.display = 'none';
+  }
+
+  const propertyList = document.getElementById('property-list');
+  propertyList.innerHTML = ''; //de fiecare data cand se face request se va sterge valoarea locala
+//se face call nou
+
   fetch(url, options)
   .then(response => response.json())
   .then(data => {
     const properties = data.data.home_search.results;
     displayProperties(properties);
     loadingSpinner.style.display = 'none';
+
+    if (loadingSpinner.style.display === 'none') {
+      searchContainer.style.display = 'flex';
+    }    
   })
   .catch(error => console.log(error));
 }
@@ -66,10 +79,37 @@ const displayProperties = (properties) => {
     card.appendChild(address);
 
 
+    card.addEventListener ('click', () => openModal(property));
     propertyList.appendChild(card);
   })
 }
 
 fetchData();
 
+const openModal = (property) => {
+  const modal = document.getElementById ('modal');
+  modal.style.display = 'block';
+  const modalProperty = document.getElementById ('modal-property');
 
+  modalProperty.innerHTML = `
+ <img src="${property.primary_photo.href}"/>
+ <h2>${property.description.beds} bed | ${property.description.baths} bath | ${property.description.sqft} sqft | ${property.description.lot_sqft} acre lot<h2/>
+  `
+}
+
+const closeModal = () => {
+  const modal = document.getElementById ('modal');
+  modal.style.display = 'none';
+}
+
+window.closeModal = closeModal; //ca html sa aiba acces la f-tiile din js
+
+const searchProperties = () => {
+  const postalCodeInput = document.getElementById('postal-code-input');
+  const requestBody = JSON.parse(options.body);
+  requestBody.postal_code = postalCodeInput.value;
+  options.body = JSON.stringify(requestBody);
+  fetchData();
+}
+
+window.searchProperties = searchProperties;
